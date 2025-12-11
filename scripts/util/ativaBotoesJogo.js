06d9b201831b462c5f1d28ad1e9e78fb13ac8db4
BrenoMorim <brenomorim13@gmail.com>
2025-12-11 16:44:53 -0300

    Feat: enabling click event on matrix cells

--- scripts/util/ativaBotoesJogo.js
+++ scripts/util/ativaBotoesJogo.js
@@ -13,7 +13,11 @@ import getJogadas from "./getJogadas.js";
 export default function ativaBotoesJogo(estado, oponente) {
   const botaoIA = document.getElementById("fazer-jogada-ia");
 
-  document.querySelectorAll(".matriz__botao").forEach(botao => {
+  [
+    ...document.querySelectorAll(".matriz__botao"), 
+    ...document.querySelectorAll(".matriz__celula") // Ao clicar na célula também funciona, para acessibilidade em celulares
+    // No caso do clique na célula, seria como adicionar a ficha na coluna daquela célula
+  ].forEach(botao => {
     // Adiciona o evento de click para os botões
     botao.addEventListener("click", () => {
       recebeAcaoDoUsuario(Number(botao.id.at(-1)), estado, oponente);
